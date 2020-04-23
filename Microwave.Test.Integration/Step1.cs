bdbc671f0b5f76492eb4ef8737f72f9ab1bd3e9b
Christina <201807962@post.au.dk>
2020-04-23 15:32:30 +0200

    step 1

--- Microwave.Test.Integration/Step1.cs
+++ Microwave.Test.Integration/Step1.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
 using NUnit.Framework;
 
 namespace Microwave.Test.Integration
@@ -10,29 +13,111 @@ namespace Microwave.Test.Integration
     [TestFixture]
     public class Step1
     {
-
+        private IOutput _output;
+        private IPowerTube _powertube;
+        private IDisplay _display;
+        private ILight _light;
+        private string _consoleOutput;
+        private StringWriter stringWriter;
         [SetUp]
         public void Setup()
         {
+            _output = new Output();
+            _powertube = new PowerTube(_output);
+            _display = new Display(_output);
+            _light = new Light(_output);
+
+            stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+                
+                
+            
+        }
+
+        //PowerTube og Output
+        [Test]
+        public void PowerTubeOutput_TurnOn()
+        {
+            _powertube.TurnOn(33);
+            _consoleOutput = stringWriter.ToString();
+            Assert.That(_consoleOutput.Contains("PowerTube works with 33"));
 
         }
 
         [Test]
-        public void TestTest()
+        public void PowerTubeOutput_TurnOff()
         {
-
+            _powertube.TurnOn(33);
+            _powertube.TurnOff();
+            _consoleOutput = stringWriter.ToString();
+            Assert.That(_consoleOutput.Contains("PowerTube turned off"));
         }
 
         [Test]
-        public void TestTest2()
+        public void PowerTubeOutput_Exp_OK()
         {
+            Assert.That(() => _powertube.TurnOn(50), Throws.Nothing);
+        }
 
+        [TestCase(101)]
+        [TestCase(0)]
+        public void PowerTubeOutput_Exp_OutOfRange(int power)
+        {
+            var ex = Assert.Catch<ArgumentOutOfRangeException>(() => _powertube.TurnOn(power));
+            StringAssert.Contains("Must be between 1 and 100 (incl.)", ex.Message);
         }
 
         [Test]
-        public void TestTest3()
+        public void PowerTubeOutput_Exp_On()
         {
+            _powertube.TurnOn(50);
+            var ex = Assert.Catch<ApplicationException>(() => _powertube.TurnOn(50));
+            StringAssert.Contains("PowerTube.TurnOn: is already on", ex.Message);
+        }
 
+
+        //Display og Output
+        [Test]
+        public void DisplayOutput_ShowTime()
+        {
+            _display.ShowTime(02,30);
+            _consoleOutput = stringWriter.ToString();
+            Assert.That(_consoleOutput.Contains("Display shows: 02:30"));
+        }
+
+        [Test]
+        public void DisplayOutput_ShowPower()
+        {
+            _display.ShowPower(40);
+            _consoleOutput = stringWriter.ToString();
+            Assert.That(_consoleOutput.Contains("Display shows: 40 W"));
+        }
+
+        [Test]
+        public void DisplayOutput_Clear()
+        {
+            _display.Clear();
+            _consoleOutput = stringWriter.ToString();
+            Assert.That(_consoleOutput.Contains("Display cleared"));
+        }
+
+
+        //Light og Output
+        [Test]
+        public void LightOutput_TurnOn()
+        {
+            _light.TurnOn();
+            _consoleOutput = stringWriter.ToString();
+            Assert.That(_consoleOutput.Contains("Light is turned on"));
+        }
+
+        [Test]
+        public void LightOutput_TurnOff()
+        {
+            _light.TurnOn();
+            _light.TurnOff();
+            _consoleOutput = stringWriter.ToString();
+            Assert.That(_consoleOutput.Contains("Light is turned off"));
         }
 
     }
---
+++ Microwave.Test.Integration/Step3.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class Step3
+    {
+        private IDoor _door;
+        private IButton _startCancelButton;
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private ILight _light;
+        private ICookController _cookcontroller;
+        private IDisplay _display;
+        private IUserInterface _userinterface;
+
+        [SetUp]
+        public void Setup()
+        {
+            _door = new Door();
+            _startCancelButton = new Button();
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _light = Substitute.For<ILight>();
+            _cookcontroller = Substitute.For<ICookController>();
+            _display = Substitute.For<IDisplay>();
+            _userinterface = new UserInterface(_powerButton,_timeButton,_startCancelButton,_door,_display,_light,_cookcontroller);
+
+        }
+
+        #region ButtonPower
+        [Test]
+        public void ButtonPowerUserInterface_Press0()
+        {
+            _display.DidNotReceive().ShowPower(50);
+        }
+
+        [Test]
+        public void ButtonPowerUserInterface_Press1()
+        {
+            _powerButton.Press();
+
+            _display.Received(1).ShowPower(50);
+        }
+        [Test]
+        public void ButtonPowerUserInterface_Press14()
+        {
+            for (int i = 0; i < 14; i++)
+            {
+                _powerButton.Press();
+            }
+
+            _display.Received(1).ShowPower(700);
+        }
+
+        [Test]
+        public void ButtonPowerUserInterface_Press15()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                _powerButton.Press();
+            }
+
+            _display.Received(2).ShowPower(50);
+        }
+        #endregion
+
+        #region ButtonTime
+
+        [Test]
+        public void ButtonTimeUserInterface_Press0()
+        {
+            _powerButton.Press();
+
+            _display.DidNotReceive().ShowTime(1, 0);
+        }
+
+        [Test]
+        public void ButtonTimeUserInterface_Press1()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+
+            _display.Received(1).ShowTime(1,0);
+        }
+
+        [Test]
+        public void ButtonTimeUserInterface_Press5()
+        {
+            _powerButton.Press();
+
+            for (int i = 0; i < 5; i++)
+            {
+                _timeButton.Press();
+            }
+            
+            _display.Received(1).ShowTime(5, 0);
+        }
+
+
+        #endregion
+
+        #region ButtonStartCancel
+
+        [Test]
+        public void ButtonStartCancelUserInterface_Press1()
+        {
+            _powerButton.Press();
+
+
+        }
+        
+
+        #endregion
+
+    }
+}
